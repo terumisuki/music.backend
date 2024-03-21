0f2c7d2b6d0b3fbe145f87b41d2c370436589bbc
Alex Wooten <alex.liveid@tek-mak.com>
2024-03-21 15:38:48 -0400

    Initial commit

---
+++ music.backend/admin/Form1.cs
@@ -0,0 +1,10 @@
+namespace admin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ music.backend/business/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace business
+{
+    public class Class1
+    {
+
+    }
+}
